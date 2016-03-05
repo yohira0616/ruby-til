bb4a449c2af9861a3589ba6dcdf86607df7300b3
yohira0616 <yukito.sys.so@gmail.com>
2016-03-06 01:52:22 +0900

    rubyは横からメソッド突っ込んだりできるらしい

---
+++ class_prac/black_magic.rb
@@ -0,0 +1,21 @@
+#すでに定義されているクラスにメソッドを追加する黒魔術
+#どうみても使うと保守性落ちるので封印推奨
+class String
+  def count_word
+    ary=self.split(/\s+/)
+    return ary.size
+  end
+end
+
+module BlackMagic
+  def doBlackMagic
+    puts "Black Magic!"
+  end
+end
+
+str="Just Another Ruby Newbie"
+p str.count_word
+
+#インスタンスに対してメソッドを追加するという暴挙
+str.extend(BlackMagic)
+str.doBlackMagic
