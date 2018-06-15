83aab55f71f0c1eb88d87025aa829a137d7f3cd1
yukito ohira <yukito.sys.so@gmail.com>
2018-06-16 00:01:51 +0900

    crowler

--- crowl.rb
+++ crowl.rb
@@ -3,17 +3,17 @@
 require 'open-uri'
 
 
-url_template = "http://img2.freshmanga.se/PUL1LlvbCQ/"
+url_template = "https://blog-imgs-103.fc2.com/k/a/n/kankoredoujin/"
 
-(1..445).each do |n|
-  url = "#{url_template}%04d.jpg" % n
+(1..21).each do |n|
+  url = "#{url_template}hmeiydum%02d.jpg" % n
   open(url) do |f|
-    filename = "%04d.jpg" % n
-    File.open("./img/#{filename}",'w') do |f2|
+    filename = "%02d.jpg" % n
+    File.open("./img/hamakaze/#{filename}",'w') do |f2|
       f2.write(f.read)
     end
 
   end
 
-  puts('%04d'% n)
+  puts('%02d'% n)
 end
